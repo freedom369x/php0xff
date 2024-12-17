ab8ae3b7ee6b34bf9dac94655655acb828f2135d
Freedom 369 <sujon0xpi@gmail.com>
2024-12-17 18:55:45 +0600

    Update key.js

--- key.js
+++ key.js
@@ -1,4 +1,4 @@
 {
-    "rc4Key": "fixedRc4Key12345",
-    "xorKey": "fixedXorKey123"
+    "rc4Key": "Zm9vYmFyMTIzNDU=", // Base64 encoded RC4 key
+    "xorKey": "YmF6cXV4MTIzNDU="  // Base64 encoded XOR key
 }
